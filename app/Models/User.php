a97fa2c3e9c6a143d9022d19d7f6e187ee7bcaba
Hsuan <hsuan@hsuan.app>
2022-02-12 14:22:04 +0800

    [Add] 新增第三方服務

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function third_parties(){
+        return $this->hasMany(ThirdParty::class);
+    }
 }
---
+++ database/migrations/2022_02_12_141203_create_third_parties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThirdPartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('third_parties', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->default('blogger');
+            $table->string('base_url');
+            $table->text('description')->default('無詳細資訊');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('third_parties');
+    }
+}
