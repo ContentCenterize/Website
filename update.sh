f45285166a76cf22d35a8c3089a0dd18b8774e4e
Hsuan <hsuan@hsuan.app>
2022-02-13 23:57:15 +0800

    [Add] update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+git stash
+git pull
+npm install
+npm run prod
+
+sudo find . -type f -exec chmod 664 {} \;
+sudo find . -type d -exec chmod 775 {} \;
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
