c9d9c435478231c401db543aa901ed94e15e1161
Hsuan <hsuan@hsuan.app>
2022-02-13 23:21:34 +0800

    [Fix] fix role

--- database/seeders/PermissionRoleSeeder.php
+++ database/seeders/PermissionRoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -20,8 +21,11 @@ class PermissionRoleSeeder extends Seeder
         Permission::create(['name' => 'read_all_posts']);
         Permission::create(['name' => 'read_all_third_parties']);
         Permission::create(['name' => 'override_robots']);
+        Permission::create(['name' => 'edit_category']);
 
         $role = Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Permission::all());
+
+        User::whereEmail('admin@admin.com')->first()->assignRole($role);
     }
 }
